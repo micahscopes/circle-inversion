6beabd90c256fb2e8322422cfaacc61223d6db81
Micah Fitch <fitchmicah@gmail.com>
2019-04-29 09:11:57 -0700

    reorganization

--- glsl_generator.py
+++ glsl_generator.py
@@ -1,8 +1,8 @@
 from alglbraic.util.cli import build_cli
 import alglbraic
-import cga
+import mathematics
 
-cli = build_cli(modules=[alglbraic, cga])
+cli = build_cli(modules=[alglbraic, mathematics])
 
 if __name__ == "__main__":
     cli()
