ed03496fc62b87f05941fefce61a79dde743470f
Micah Fitch <fitchmicah@gmail.com>
2019-05-07 14:50:14 -0500

    fuzzy bunny

--- inside-out-bunny.js
+++ inside-out-bunny.js
@@ -1,7 +1,7 @@
 import glLib from './gl/lib.glsl'
 const regl = require('regl')()
 const camera = require('regl-camera')(regl, {
-  center: [0, 0, 0],
+  center: [0, 0.1, 0.05],
   up: [0,-1,0]
 })
 
@@ -13,29 +13,41 @@ let gl = function(s){
 const bunny = require('bunny')
 const normals = require('angle-normals')
 
+const NOISE_SIZE = 1/5
+let randomPoint = (size) => () => [
+  Math.random()*size,
+  Math.random()*size,
+  Math.random()*size
+]
+
+let makeBunnyNoise = () =>bunny.positions.map(randomPoint(NOISE_SIZE))
+let bunnyNoise = regl.buffer({
+  data: makeBunnyNoise()
+})
+
 window.bunny = bunny
 window.normals = normals
 
-const RAD = 4.0
+const RAD = 1.0
 
 const drawBunny = regl({
   vert: gl`
     uniform vec3 a, b, c, d;
     uniform mat4 projection, view;
-    attribute vec3 position, normal;
+    attribute vec3 position, noise, normal;
     varying vec3 vnormal;
 
     void main () {
       CGA3 sphere = sphere(a,b,c,d);
-      vec3 x = reflect_glsl(position, sphere);
+      vec3 x = reflect_glsl(position+noise, sphere);
       vnormal = normal;
-      gl_PointSize = 10.0;
+      gl_PointSize = 4.0;
       gl_Position = projection * view * vec4(x, 1.0);
     }`,
   frag: gl`
     varying vec3 vnormal;
     void main () {
-      gl_FragColor = vec4(vnormal, 0.4);
+      gl_FragColor = vec4(vnormal+vec3(0.4,0.4,0.4), 0.8);
     }`,
   blend: {
     enable: true,
@@ -49,31 +61,30 @@ const drawBunny = regl({
       rgb: 'add',
       alpha: 'add'
     },
-    color: [0, 1, 1, 0.5]
+    color: [0, 0, 0, 0]
   },
   attributes: {
     position: bunny.positions,
-      // primitive: 'points',
+    noise: bunnyNoise,
     normal: normals(bunny.cells, bunny.positions)
   },
   count: bunny.positions.length,
   primitive: 'points',
-
-
   uniforms: {
     a: [-RAD,0,0],
     b: [RAD,0,0],
     c: [0,RAD,0],
     d: [0,0,RAD],
   },
-  // elements: bunny.cells
 })
 
 regl.frame(() => {
   camera((state) => {
-    if (!state.dirty) return;
-    // console.log(state)
-    regl.clear({color: [0, 0, 0, 1]})
+    // if (!state.dirty) return;
+    bunnyNoise({
+      data: makeBunnyNoise()
+    })
+    // regl.clear({color: [0, 0, 0, 1]})
     drawBunny()
   })
 })
