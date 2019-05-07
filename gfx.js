c0bf760ce46af77276b0fdb96614abf206568d6c
Micah Fitch <fitchmicah@gmail.com>
2019-05-07 14:30:46 -0500

    bunny points inversion

--- gfx.js
+++ gfx.js
@@ -1,7 +1,8 @@
 import glLib from './gl/lib.glsl'
 const regl = require('regl')()
 const camera = require('regl-camera')(regl, {
-  center: [0, 2.5, 0]
+  center: [0, 0, 0],
+  up: [0,-1,0]
 })
 
 let gl = function(s){
@@ -12,10 +13,10 @@ let gl = function(s){
 const bunny = require('bunny')
 const normals = require('angle-normals')
 
-window.bunny = bunny;
-window.normals = normals;
+window.bunny = bunny
+window.normals = normals
 
-const RAD = 4.0;
+const RAD = 4.0
 
 const drawBunny = regl({
   vert: gl`
@@ -28,29 +29,50 @@ const drawBunny = regl({
       CGA3 sphere = sphere(a,b,c,d);
       vec3 x = reflect_glsl(position, sphere);
       vnormal = normal;
+      gl_PointSize = 10.0;
       gl_Position = projection * view * vec4(x, 1.0);
     }`,
   frag: gl`
     varying vec3 vnormal;
     void main () {
-      gl_FragColor = vec4(abs(vnormal), 1.0);
+      gl_FragColor = vec4(vnormal, 0.4);
     }`,
+  blend: {
+    enable: true,
+    func: {
+      srcRGB: 'src alpha',
+      srcAlpha: 1,
+      dstRGB: 'one minus src alpha',
+      dstAlpha: 1
+    },
+    equation: {
+      rgb: 'add',
+      alpha: 'add'
+    },
+    color: [0, 1, 1, 0.5]
+  },
   attributes: {
     position: bunny.positions,
+      // primitive: 'points',
     normal: normals(bunny.cells, bunny.positions)
   },
+  count: bunny.positions.length,
+  primitive: 'points',
+
+
   uniforms: {
     a: [-RAD,0,0],
     b: [RAD,0,0],
     c: [0,RAD,0],
     d: [0,0,RAD],
   },
-  elements: bunny.cells
+  // elements: bunny.cells
 })
 
 regl.frame(() => {
   camera((state) => {
     if (!state.dirty) return;
+    // console.log(state)
     regl.clear({color: [0, 0, 0, 1]})
     drawBunny()
   })
